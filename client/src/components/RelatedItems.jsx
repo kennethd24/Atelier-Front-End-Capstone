8f028ad880b72da893b5ad1e3ca0d0bf9419c257
Andrew Munoz <ajmunoz411@gmail.com>
2021-06-08 11:51:46 -0700

    Removed comments from Related Items top component

--- client/src/components/RelatedItems.jsx
+++ client/src/components/RelatedItems.jsx
@@ -1,43 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import RelatedItemsComp from './related_components/RelatedItems.jsx';
-import YourOutfitComp from './related_components/YourOutfit.jsx';
+import RelatedItemsComp from './related_components/RelatedItems';
+import YourOutfitComp from './related_components/YourOutfit';
 
 const RelatedItems = (props) => {
   const { currentItem } = props;
-
-  // const [relatedIds, setRelatedIds] = useState([]);
-  // const [relatedItems, setRelatedItems] = useState([]);
-  // const [yourOutfit, setYourOutfit] = useState([]);
-
-  // const getRelatedItems = (arr) => {
-  //   arr.forEach((itemId) => (
-  //     axios.get(`/api/products/${itemId}`)
-  //       .then((results) => {
-  //         setRelatedItems(relatedItems.push(results.data));
-  //       })
-  //       .catch((err) => {
-  //         console.log('error in getrelatedItems', err);
-  //       })
-  //   ));
-  // };
-
-  // const getRelatedIds = (id) => {
-  //   axios.get(`/api/related/${id}`)
-  //     .then((results) => {
-  //       setRelatedIds(relatedIds.push(results.data));
-  //       getRelatedItems(results.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log('error in getRelatedIds', err);
-  //     });
-  // };
-
-
-
-  // useEffect(() => {
-  //   getRelatedIds(currentItem.id);
-  // }, [currentItem]);
 
   return (
     <div id="related-outfit-wrapper">
