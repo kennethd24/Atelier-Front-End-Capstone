3290294b9ae8e061378019cf65d513f75b94b5c6
Andrew Munoz <ajmunoz411@gmail.com>
2021-06-11 12:34:45 -0700

    Refactor of styles in progress

--- client/src/components/related_components/ProductCard.jsx
+++ client/src/components/related_components/ProductCard.jsx
@@ -49,7 +49,7 @@ const ProductCard = (props) => {
   };
 
   const getStyles = (id, selected) => {
-    console.log('getStyles Fired');
+    // console.log('getStyles Fired');
     axios.get(`/api/products/${id}/styles`)
       .then((results) => {
         const resultsArr = results.data.results;
