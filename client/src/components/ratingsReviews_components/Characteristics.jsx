f2a7d4ce55abe22669665373a853eeca1879464c
Kenneth Doan <kennethxdoan@gmail.com>
2021-06-13 15:52:40 -0700

    Fixed linter

--- client/src/components/ratingsReviews_components/Characteristics.jsx
+++ client/src/components/ratingsReviews_components/Characteristics.jsx
@@ -3,9 +3,7 @@ import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const Characteristics = (props) => {
   const { characteristic, objValue } = props;
-  // console.log(objValue[characteristic].value);
   const now = (objValue[characteristic].value / 5) * 100;
-  console.log(now);
   return (
     <div className="characteristic-entry">
       <div className="characteristic-left">
