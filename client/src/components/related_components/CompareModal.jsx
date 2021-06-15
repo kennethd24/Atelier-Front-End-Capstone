94159c69d45cba6d5dc2c0ada19ffcc2fff15803
Andrew Munoz <ajmunoz411@gmail.com>
2021-06-14 20:29:19 -0700

    Added more features to comparison table in modal

--- client/src/components/related_components/CompareModal.jsx
+++ client/src/components/related_components/CompareModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Modal, Table,
 } from 'react-bootstrap';
@@ -10,6 +10,39 @@ const CompareModal = (props) => {
     selectedItem,
     relatedItem,
   } = props;
+
+  console.log('selectedItem', selectedItem);
+  console.log('relatedItem', relatedItem);
+
+  const [selFeats, setSelFeats] = useState([]);
+  const [relFeats, setRelFeats] = useState([]);
+
+  // useEffect(() => {
+  //   if (selectedItem.category === relatedItem.category) {
+
+  //   }
+  // }, [selectedItem, relatedItem]);
+
+  useEffect(() => {
+    if (selectedItem.features) {
+      setSelFeats([...selectedItem.features]);
+    }
+  }, [selectedItem]);
+
+  useEffect(() => {
+    if (relatedItem.features) {
+      setRelFeats([...relatedItem.features]);
+    }
+  }, [relatedItem]);
+  // const checkFeatures = (selArr, relArr) => {
+  //   if (selArr >= relArr) {
+  //     selArr.forEach((selFeat) => {
+  //       relArr.forEach((relFeat) => {
+
+  //       })
+  //     }
+  //   }
+  // }
 
   return (
     <Modal
@@ -28,20 +61,62 @@ const CompareModal = (props) => {
       </Modal.Header>
       <Modal.Body>
         <table>
-          <tr>
-            <th colSpan="1.5">{selectedItem.name}</th>
-            <th colSpan="1.5">{relatedItem.name}</th>
-          </tr>
-          <tr>
-            <td>{selectedItem.price}</td>
-            <td>PRICE</td>
-            <td>{relatedItem.price}</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>RATING</td>
-            <td>5</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>{selectedItem.name}</th>
+              <th> </th>
+              <th>{relatedItem.name}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{selectedItem.category}</td>
+              <td>CATEGORY</td>
+              <td>{relatedItem.category}</td>
+            </tr>
+            <tr>
+              <td>{selectedItem.price}</td>
+              <td>PRICE</td>
+              <td>{relatedItem.price}</td>
+            </tr>
+            <tr>
+              <td>???</td>
+              <td>RATING</td>
+              <td>???</td>
+            </tr>
+            <tr>
+              <td>{selectedItem.description}</td>
+              <td>DESCRIPTION</td>
+              <td>{relatedItem.description}</td>
+            </tr>
+            <tr>
+              <td>{selectedItem.slogan}</td>
+              <td>SLOGAN</td>
+              <td>{relatedItem.slogan}</td>
+            </tr>
+            {selFeats.map((featObj) => (
+              <tr>
+                <td>YES</td>
+                <td>
+                  {featObj.feature}
+                  -
+                  {featObj.value}
+                </td>
+                <td>NO</td>
+              </tr>
+            ))}
+            {relFeats.map((featObj) => (
+              <tr>
+                <td>NO</td>
+                <td>
+                  {featObj.feature}
+                  -
+                  {featObj.value}
+                </td>
+                <td>YES</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </Modal.Body>
     </Modal>
