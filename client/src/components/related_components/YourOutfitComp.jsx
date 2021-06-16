b0ba5872e351b18489eb82bd290bd9d2feb1daf6
Andrew Munoz <ajmunoz411@gmail.com>
2021-06-16 10:43:42 -0700

    Implemented local storage persistence through page re-loads/tab closing/etc w/local storage

--- client/src/components/related_components/YourOutfitComp.jsx
+++ client/src/components/related_components/YourOutfitComp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination } from 'swiper';
 import 'swiper/swiper-bundle.css';
@@ -13,8 +13,25 @@ const YourOutfitComp = (props) => {
     selectedDefault,
   } = props;
 
-  const [yourOutfit, setYourOutfit] = useState([]);
+  // console.log('yourOutfit', yourOutfit);
+
+  // const [yourOutfit, setYourOutfit] = useState([]);
+  const [yourOutfit, setYourOutfit] = useState(
+    // console.log(JSON.parse(localStorage.getItem('yourOutfitInLocal')));
+    JSON.parse(localStorage.getItem('yourOutfitInLocal')) || [],
+  );
+
   const [outfitIds, setOutfitIds] = useState([]);
+  // const [value, setValue] = React.useState(
+  //   localStorage.getItem('valueInLocal') || '',
+  // );
+
+  // React.useEffect(() => {
+  //   localStorage.setItem('valueInLocal', value);
+  // }, [value]);
+  useEffect(() => {
+    localStorage.setItem('yourOutfitInLocal', JSON.stringify(yourOutfit));
+  }, [yourOutfit]);
 
   const handleAdd = () => {
     if (!outfitIds.includes(selectedItem.id)) {
@@ -25,6 +42,8 @@ const YourOutfitComp = (props) => {
       selectedItem.salePrice = selectedDefault.sale_price;
       selectedItem.origPrice = selectedDefault.original_price;
       setYourOutfit([...yourOutfit, selectedItem]);
+      // localStorage.setItem('yourOutfitInLocal', JSON.stringify(yourOutfit));
+      // console.log(JSON.parse(localStorage.getItem('yourOutfitInLocal')));
     }
   };
 
@@ -40,6 +59,7 @@ const YourOutfitComp = (props) => {
 
   return (
     <div className="your-outfit-carousel">
+      {/* <div>{value}</div> */}
       <Swiper
         slidesPerView={3.5}
         spaceBetween={25}
