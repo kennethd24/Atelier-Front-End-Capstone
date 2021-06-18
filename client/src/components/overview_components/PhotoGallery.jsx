93d80813c4f781ac7a348b79e45c2ed167df6bea
Chris Wu <cwu0569@gmail.com>
2021-06-17 22:35:53 -0700

    created component to handle both default and expanded views

--- client/src/components/overview_components/PhotoGallery.jsx
+++ client/src/components/overview_components/PhotoGallery.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import SwiperCore, { Navigation, Thumbs } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
+import DefaultView from './DefaultView';
+import ExpandedView from './ExpandedView';
 
 const PhotoGallery = (props) => {
   const { currentStyle } = props;
 
+  const [showModal, setShowModal] = useState(false);
   const [mainIndex, setMainIndex] = useState(0);
   const [mainSwiper, setMainSwiper] = useState();
   const [thumbsSwiper, setThumbsSwiper] = useState();
-
-  SwiperCore.use([Navigation, Thumbs]);
+  const [expandedSwiper, setExpandedSwiper] = useState();
 
   const slideTo = () => {
     mainSwiper.slideTo(mainIndex);
@@ -23,47 +22,24 @@ const PhotoGallery = (props) => {
   }, [currentStyle]);
 
   return (
-    <div className="gallery-overlay">
-      <div className="thumb-swiper">
-        <button type="button" className="arrow-btn up-btn">
-          <i className="fas fa-chevron-up" />
-        </button>
-        <Swiper
-          spaceBetween={15}
-          slidesPerView={7}
-          direction={'vertical'}
-          onSwiper={setThumbsSwiper}
-          navigation={{
-            nextEl: '.down-btn',
-            prevEl: '.up-btn',
-          }}
-        >
-          {currentStyle.photos.map((photoObj, index) => (
-            <SwiperSlide key={index} className="thumbnail-slide">
-              <img className="thumbnail-pic" src={photoObj.thumbnail_url} alt={`Slide ${index}`} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <button type="button" className="arrow-btn down-btn">
-          <i className="fas fa-chevron-down" />
-        </button>
-      </div>
-
-      <Swiper
-        slidesPerView={1}
-        thumbs={{ swiper: thumbsSwiper }}
-        onSlideChange={(swiper) => setMainIndex(swiper.activeIndex)}
-        onSwiper={setMainSwiper}
-        className="info-swiper"
-        navigation
-      >
-        {currentStyle.photos.map((photoObj, index) => (
-          <SwiperSlide key={index}>
-            <img className="slider-pic" src={photoObj.url} alt={`Slide ${index}`} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    <>
+      <DefaultView
+        currentStyle={currentStyle}
+        setMainIndex={setMainIndex}
+        setMainSwiper={setMainSwiper}
+        thumbsSwiper={thumbsSwiper}
+        setThumbsSwiper={setThumbsSwiper}
+        setShowModal={setShowModal}
+      />
+      <ExpandedView
+        currentStyle={currentStyle}
+        setExpandedSwiper={setExpandedSwiper}
+        showModal={showModal}
+        setShowModal={setShowModal}
+        mainIndex={mainIndex}
+        mainSwiper={mainSwiper}
+      />
+    </>
   );
 };
 
